13d7b77dec6fcc46a181abfce6b08f826bf85ee1
bradleyclarke <bradley.j.clarke@tesco.com>
2019-08-20 10:29:27 +0100

    initial commit

---
+++ src/main/kotlin/com/plasticbashers/teamday/TeamdayApplication.kt
@@ -0,0 +1,11 @@
+package com.plasticbashers.teamday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamdayApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeamdayApplication>(*args)
+}
---
+++ src/test/kotlin/com/plasticbashers/teamday/TeamdayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.plasticbashers.teamday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamdayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
